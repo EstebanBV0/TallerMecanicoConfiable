10859bccd7931af03d794da54d3f28f4deecc7e1
LUIS <luifer8610@hotmail.com>
2022-09-16 05:48:39 -0500

    form Mecanico

--- MecanicoConfiable.App.Servicios/Pages/Services/Car/AddMecanico.cshtml.cs
+++ MecanicoConfiable.App.Servicios/Pages/Services/Car/AddMecanico.cshtml.cs
@@ -8,7 +8,7 @@ namespace MecanicoConfiable.App.Servicios
     public class AddMecanicoModel : PageModel
     {
     private readonly IRepositorioVehiculo _repoVehiculo = new RepositorioVehiculo(new Persistencia.AppContext());
-
+    private readonly IRepositorioMecanico _repoMecanico = new RepositorioMecanico(new Persistencia.AppContext());
     private readonly ILogger<EditMecanicoModel> _logger;
 
     public AddMecanicoModel(ILogger<EditMecanicoModel> logger)
@@ -18,9 +18,11 @@ namespace MecanicoConfiable.App.Servicios
       
     [BindProperty]
     public Vehiculo Vehiculo { get; set; }
+    public IEnumerable<Mecanico> Mecanico { get; set; }
     
       public  IActionResult OnGet( string Placa)
         {
+            Mecanico = _repoMecanico.GetAll();
             Vehiculo = _repoVehiculo.GetVehiculoPlaca(Placa);
 
             if(Vehiculo==null){
