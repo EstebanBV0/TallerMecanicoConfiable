c72b4341182062f1caee4b0b54ef7ab593118450
LUIS <luifer8610@hotmail.com>
2022-09-17 20:19:07 -0500

    commit17092019

--- MecanicoConfiable.App.Servicios/Pages/Services/Car/DetailVehiculo.cshtml.cs
+++ MecanicoConfiable.App.Servicios/Pages/Services/Car/DetailVehiculo.cshtml.cs
@@ -8,6 +8,7 @@ namespace MecanicoConfiable.App.Servicios;
 public class DetailVehiculoModel : PageModel
 {
     private readonly IRepositorioVehiculo _repoVehiculo = new RepositorioVehiculo(new Persistencia.AppContext());
+    private readonly IRepositorioMecanico _repoMecanico = new RepositorioMecanico(new Persistencia.AppContext());
 
     
 
@@ -20,10 +21,12 @@ public class DetailVehiculoModel : PageModel
 
     [BindProperty]
     public Vehiculo Vehiculo { get; set; }
+    public Mecanico Mecanico { get; set; }
     
       public  IActionResult OnGet( int IdVehiculo)
         {
             Vehiculo = _repoVehiculo.GetVehiculoId(IdVehiculo);
+            Mecanico = _repoMecanico.GetMecanicoId(Vehiculo.IdMecanico);
 
             if(Vehiculo==null){
                 return RedirectToPage("./NotFound");
